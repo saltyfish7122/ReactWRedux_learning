c9c5d513ec4c53ff55b417ae8e8962bb45a766fc
saltyfish7122 <xinqiuhuang@gmail.com>
2020-06-18 02:56:40 -0500

    Update index.js

--- Components/src/index.js
+++ Components/src/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from "./CommentDetails";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
 
 const App = () => {
     return(
             <div className = "ui container components">
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
-                <CommentDetail />
+                <ApprovalCard>
+                    <div>
+                        <h4>Warning!</h4>
+                        Are you sure you want to do this?
+                    </div>
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail
+                                author="Sam"
+                               timeAgo = "Today at 4:45PM"
+                               comment = "Nice posts!"
+                               avatar = {faker.image.avatar()}/>
+
+                    </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail
+                                author="Alex"
+                               timeAgo = "Today at 2:00AM "
+                               comment = "I love it!"
+                               avatar = {faker.image.avatar()}/>
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail
+                                author="Jane"
+                               timeAgo = "Yesterday at 5:00PM"
+                               comment = "Great!"
+                               avatar = {faker.image.avatar()}/>
+                </ApprovalCard>
             </div>
         );
 };
