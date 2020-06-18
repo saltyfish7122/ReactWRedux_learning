d46f6e071fd25b361c621a03c169821a792f5e29
saltyfish7122 <xinqiuhuang@gmail.com>
2020-06-18 02:56:20 -0500

    Add files via upload

---
+++ Components/src/ApprovalCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ApprovalCard = props =>
+{
+    return(
+        <div className="ui card">
+            <div className="content">
+                {props.children}
+            </div>
+            <div className="extra content">
+                <div className="ui two buttons">
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Reject</div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+/* make use of this component in other location inside this project*/
+export default ApprovalCard;
